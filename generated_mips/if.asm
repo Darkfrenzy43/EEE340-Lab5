f9300e49f80c3c5d2e3b6693e3df5f45ae500256
Aaron Brown <snakeskipper@proton.me>
2023-04-02 15:36:22 -0400

    made test for if

--- generated_mips/if.asm
+++ generated_mips/if.asm
@@ -4,7 +4,9 @@ true_string: .asciiz "true"
 false_string: .asciiz "false"
     
 string_2: .asciiz "\n"
-string_5: .asciiz "done"
+string_5: .asciiz "after false if\n"
+string_6: .asciiz "\n"
+string_9: .asciiz "done"
 
 .text
 
@@ -21,6 +23,9 @@ main:
 
 move $fp $sp
 
+addiu  $sp $sp -4
+li     $t0 0
+sw     $t0 -4($fp)
 
 li     $t0 1
 beqz   $t0 endif_0
@@ -52,6 +57,42 @@ endif_3:
 endelse_4:
 
 la     $t0 string_5
+move   $a0 $t0
+li     $v0 4
+syscall
+
+lw   $t0  -4($fp)
+addiu  $sp $sp -4 
+sw     $t0 0($sp) 
+li     $t0 1
+lw     $s1 0($sp) 
+add    $t0 $s1 $t0 
+addiu  $sp $sp 4
+
+addiu  $sp $sp -4 
+sw     $t0 0($sp) 
+li     $t0 1
+lw     $s1 0($sp) 
+seq    $t0 $s1 $t0 
+addiu  $sp $sp 4
+
+beqz   $t0 endif_7
+lw   $t0  -4($fp)
+move   $a0 $t0
+li     $v0 1
+syscall
+
+la     $t0 string_6
+move   $a0 $t0
+li     $v0 4
+syscall
+
+b endelse_8
+endif_7:
+
+endelse_8:
+
+la     $t0 string_9
 move   $a0 $t0
 li     $v0 4
 syscall
